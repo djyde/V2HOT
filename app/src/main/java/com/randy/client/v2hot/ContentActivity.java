cccd3c7aa1f22a8e6f907f78d5aca18a04ee2c1f
Randy <djyde520@gmail.com>
2014-08-29 09:56:24 +0800

    1.0 release

---
+++ app/src/main/java/com/randy/client/v2hot/ContentActivity.java
@@ -0,0 +1,116 @@
+package com.randy.client.v2hot;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class ContentActivity extends Activity {
+
+
+    private TextView tv_title;
+    private TextView tv_content;
+    private ProgressBar pb_load;
+    private ListView lv_reply;
+    private List<HashMap<String,String>> repliesList = new ArrayList<HashMap<String, String>>();
+    private TextView headertitle;
+    private TextView headercontent;
+    private View header;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+
+        //获取相应的topic id
+        Intent getIntent = getIntent();
+        String id = getIntent.getStringExtra("id");
+        final String title = getIntent.getStringExtra("title");
+        header = getLayoutInflater().inflate(R.layout.topic_header,null);
+        headertitle = (TextView)header.findViewById(R.id.header_title);
+        headercontent = (TextView)header.findViewById(R.id.header_content);
+        headertitle.setText(title);
+
+        RequestQueue queue = Volley.newRequestQueue(ContentActivity.this);
+        //根据API(http://github.com/djyde/v2ex-api)获取topic内容.
+        JsonArrayRequest topicInfoRequest = new JsonArrayRequest("http://www.v2ex.com/api/topics/show.json?id=" + id,new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                pb_load = (ProgressBar)ContentActivity.this.findViewById(R.id.pb_load);
+                pb_load.setVisibility(View.GONE);
+                try {
+                    //获取topic内容
+                    String content = response.getJSONObject(0).getString("content");
+                    headercontent.setText(content);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //处理错误
+                Toast.makeText(ContentActivity.this,error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //获取回复
+        JsonArrayRequest replyRequest = new JsonArrayRequest("http://www.v2ex.com/api/replies/show.json?topic_id=" + id,new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                //遍历所有回复
+                for (int i=0;i<response.length();i++){
+                    HashMap<String,String> map = new HashMap<String, String>();
+                    try {
+                        map.put("content",response.getJSONObject(i).getString("content"));
+                        map.put("username",response.getJSONObject(i).getJSONObject("member").getString("username"));
+                        repliesList.add(map);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }//遍历结束
+                lv_reply = (ListView)ContentActivity.this.findViewById(R.id.lv_reply);
+                lv_reply.addHeaderView(header);
+                SimpleAdapter replyAdapter = new SimpleAdapter(ContentActivity.this,repliesList,R.layout.reply_item,new String[]{"content","username"},new int[]{R.id.tv_reply,R.id.tv_username});
+                lv_reply.setAdapter(replyAdapter);
+            }
+        },new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ContentActivity.this,"请检查网络",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        queue.add(topicInfoRequest);
+        queue.add(replyRequest);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/randy/client/v2hot/TitleListAcitity.java
@@ -0,0 +1,136 @@
+package com.randy.client.v2hot;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class TitleListAcitity extends Activity {
+
+    private ListView lv_title;
+    private TextView tv;
+    private ProgressBar pb_load;
+    private ArrayList<String> idList = new ArrayList<String>();
+    private ArrayList<String> titleList = new ArrayList<String>();
+    private List<HashMap<String,String>> topicList = new ArrayList<HashMap<String, String>>();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_list);
+        //创建volley请求队列
+        RequestQueue queue= Volley.newRequestQueue(TitleListAcitity.this);
+
+        //使用Volley获取V2EX首页源代码
+        StringRequest request = new StringRequest("http://v2ex.com",new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //用Jsoup解析源代码
+                Document doc = Jsoup.parse(response);
+                //用Jsoup选择器取出title和topic id
+                Elements titles = doc.select("span.item_hot_topic_title > a");
+                Elements ids = doc.select("span.item_hot_topic_title > a");
+                for (Element id:ids){
+                    idList.add(id.attr("href").split("/")[2]);
+                }
+                for (Element title:titles){
+                    titleList.add(title.text());
+                }
+                for (int i=0; i < idList.size();i++){
+                    HashMap<String,String> map = new HashMap<String, String>();
+                    map.put("id",idList.get(i));
+                    map.put("title",titleList.get(i));
+                    topicList.add(map);
+                }
+
+                //适配器
+                lv_title = (ListView)TitleListAcitity.this.findViewById(R.id.lv_title);
+                SimpleAdapter topicAdapter = new SimpleAdapter(TitleListAcitity.this,topicList,R.layout.topic_item,new String[]{"title"},new int[]{R.id.tv_title});
+                lv_title.setAdapter(topicAdapter);
+                lv_title.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Intent intent = new Intent(TitleListAcitity.this,ContentActivity.class);
+                        HashMap<String,String> info = (HashMap<String, String>) lv_title.getItemAtPosition(i);
+                        //取id和title传递到另一个activity
+                        String id = info.get("id");
+                        String title = info.get("title");
+                        intent.putExtra("id",id);
+                        intent.putExtra("title",title);
+                        startActivity(intent);
+                    }
+                });
+
+                //加载结束后progressbar消失
+                pb_load = (ProgressBar)findViewById(R.id.pb_load);
+                pb_load.setVisibility(View.GONE);
+
+            }
+        },new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //错误提示处理
+                Toast.makeText(TitleListAcitity.this,"请检查网络",Toast.LENGTH_LONG).show();
+            }
+        }){
+            //重写getHeader方法，修改User agent
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> headers = new HashMap<String,String>();
+                headers.put("User-agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36");
+                return headers;
+            }
+        };
+
+        queue.add(request);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.title_list_acitity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(TitleListAcitity.this,AboutActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
