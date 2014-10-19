98009ce8f10bd0d32dbb3d9cdd0aacbe468dbd8c
XiNGRZ <chenxingyu92@gmail.com>
2014-10-19 14:30:24 +0800

    使用 Gson 解析 JSON

--- app/src/main/java/com/randy/client/v2hot/TitleListActivity.java
+++ app/src/main/java/com/randy/client/v2hot/TitleListActivity.java
@@ -4,26 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import com.v2ex.api.GsonRequest;
+import com.v2ex.api.Topic;
+import com.v2ex.api.TopicList;
 
 public class TitleListActivity extends ActionBarActivity {
 
@@ -34,32 +33,36 @@ public class TitleListActivity extends ActionBarActivity {
 
         final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.pull);
 
-        final List<HashMap<String, String>> topicList = new ArrayList<HashMap<String, String>>();
+        final ArrayAdapter<Topic> topicsAdapter = new ArrayAdapter<Topic>(this, 0) {
 
-        final SimpleAdapter topicsAdapter = new SimpleAdapter(
-                this,
-                topicList,
-                R.layout.topic_item,
-                new String[]{"title"},
-                new int[]{R.id.title}
-        );
+            private final LayoutInflater inflater = LayoutInflater.from(getContext());
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = inflater.inflate(R.layout.topic_item, parent, false);
+                }
+
+                TextView title = (TextView) convertView.findViewById(R.id.title);
+                title.setText(getItem(position).title);
+
+                return convertView;
+            }
+
+        };
 
         final ListView topicsView = (ListView) findViewById(R.id.topics);
 
         topicsView.setAdapter(topicsAdapter);
         topicsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                HashMap<String, String> info = (HashMap<String, String>) topicsView.getItemAtPosition(i);
-                String id = info.get("id");
-                String title = info.get("title");
-                String username = info.get("username");
-                String url = info.get("url");
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Topic topic = topicsAdapter.getItem(position);
                 Intent intent = new Intent(TitleListActivity.this, ContentActivity.class);
-                intent.putExtra("id", id);
-                intent.putExtra("title", title);
-                intent.putExtra("username", username);
-                intent.putExtra("url", url);
+                intent.putExtra("id", topic.id);
+                intent.putExtra("title", topic.title);
+                intent.putExtra("username", topic.member.username);
+                intent.putExtra("url", topic.url);
                 startActivity(intent);
             }
         });
@@ -68,26 +71,13 @@ public class TitleListActivity extends ActionBarActivity {
         final RequestQueue queue = Volley.newRequestQueue(this);
 
         //根据API获取热议主题
-        final JsonArrayRequest request = new JsonArrayRequest("http://www.v2ex.com/api/topics/hot.json",
-                new Response.Listener<JSONArray>() {
+        final GsonRequest<TopicList> request = new GsonRequest<TopicList>(
+                Request.Method.GET, "https://www.v2ex.com/api/topics/hot.json", TopicList.class,
+                new Response.Listener<TopicList>() {
                     @Override
-                    public void onResponse(JSONArray response) {
-                        topicList.clear();
-
-                        //取id、title、username
-                        for (int i = 0; i < response.length(); i++) {
-                            HashMap<String, String> map = new HashMap<String, String>();
-                            try {
-                                map.put("id", response.getJSONObject(i).getString("id"));
-                                map.put("title", response.getJSONObject(i).getString("title"));
-                                map.put("username", response.getJSONObject(i).getJSONObject("member").getString("username"));
-                                map.put("url", response.getJSONObject(i).getString("url"));
-                                topicList.add(map);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
+                    public void onResponse(TopicList response) {
+                        topicsAdapter.clear();
+                        topicsAdapter.addAll(response);
                         topicsAdapter.notifyDataSetChanged();
                         swipeRefreshLayout.setRefreshing(false);
                     }
